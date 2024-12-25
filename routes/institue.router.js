f4b6cc95418ba0ccf7cc13f79879116858b341b5
sharma-rajesh12 <yadavrajesh1344@gmail.com>
2024-12-25 16:42:11 +0530

    Remove Router folder

--- routes/institue.router.js
+++
@@ -1,9 +0,0 @@
-// const { Router } = require("express")
-// const { createInstituteController } = require('../controller/intitute.controller');
-// // const { createInstituteSchema } = require('../middleware/institute.requestSchema');
-
-// const route = Router();
-
-// route("/createInstitute", createInstituteController)
-
-// module.exports = route;
